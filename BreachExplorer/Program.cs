65920ce19acce87aff1110e47e49cb610b769afd
Vlad Pocris <vpocris@gmail.com>
2024-11-25 01:35:25 +0000

    BarChartTop15Breaches

--- BreachExplorer/Program.cs
+++ BreachExplorer/Program.cs
@@ -13,6 +13,8 @@ namespace BreachExplorer
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddBlazorBootstrap();
+
             await builder.Build().RunAsync();
         }
     }
